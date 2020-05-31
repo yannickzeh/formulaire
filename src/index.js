c0e69dec2091618e1b8471c91fc7df098e461526
yannickzeh <zehyannick@hotmail.fr>
2020-05-31 17:27:45 +0200

    Ajout de l'évènement submit qui va pouvoir traiter les informations du formulaire + initialisation du compte permettant de se connecter

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import './style.css';
             
 moment.locale('fr')
 
+const email = 'user@user.fr'
+const password = 'password'
+
+
+//time
+const loadtime = 800;
+
+
+//////////////////
+/// various functions
+
+const onFormSubmit = e => {
+    e.preventDefault(); // Annule l'action par défaut
+
+
+    setTimeout(() => {
+        // Récupère les données du formulaire
+        const data = new FormData(e.target);
+        const response = ProcessDataForm(data);
+    }, loadtime); // 1 seconde
+}
+
+document.querySelector('#form').addEventListener('submit',onFormSubmit);
 
 const handleErrorMessage = () => {
   document.querySelector('.errorconnexion').classList.remove('invisible');
